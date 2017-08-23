9c1a531430326c13c39fb70278e3adf930b8ce89
WizBit <dean@monkiezone.com>
2017-08-23 18:10:47 +0100

    Wind Inclusion
    Cleaned up Hierarchy and added a Wind element to player.

---
+++ 1.0/Assets/WindController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WindController : MonoBehaviour {
+
+    //enum WindDirection { Left, Right };
+
+    public float windSpeed;
+
+    Rigidbody rb;
+    bool windForce = false;
+    int direction;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.P))
+        {
+            windForce = !windForce;
+            direction = Random.Range(0, 2);
+        }
+
+        StartWindForce();
+	}
+
+    private void StartWindForce()
+    {
+        if (windForce)
+        {
+            if (direction == 0)
+            {
+                rb.AddForce(windSpeed, 0, 0, ForceMode.Force);
+            }
+            else
+            {
+                rb.AddForce(-windSpeed, 0, 0, ForceMode.Force);
+            }
+
+        }
+    }
+}
